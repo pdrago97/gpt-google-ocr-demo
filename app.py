4197eb4a5f2b07c67d7d6ff72959410f78553677
pedro_reichow <pedroreichow3@gmail.com>
2023-12-20 15:56:54 -0300

    increment prompt

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def generate_response(prompt):
         model="gpt-3.5-turbo-1106",
         response_format={ "type": "json_object" },
         messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output a optimized JSON."},
+            {"role": "system", "content": "You are a helpful assistant designed to output a optimized JSON for medical documents, if the image is not related to medical documents, return a json with a negative response, like 'valid': not valid."},
             {"role": "user", "content": f"{prompt}"}
         ],
     )
